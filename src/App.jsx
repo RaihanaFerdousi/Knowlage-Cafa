cc8314192eac550bd1f60a5e04bccd128b354306
Raihana Ferdousi <raihanaferdousi110@gmail.com>
2024-10-22 10:31:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blog from './components/Blogs/Blog';
+import Bookmarks from './components/Bookmarks/Bookmarks';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+      <>
+        <Header></Header>
+        <div className=' md:flex'>
+          <Blog></Blog>
+          <Bookmarks></Bookmarks>
+        </div>
+      </>
+  );
+}
+
+export default App
